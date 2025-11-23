d007952f103cb341043fe561b8e7778266e5842e
zhangchugao <1259563100@qq.com>
2025-11-23 21:16:02 +0800

    删除提交到github上的node_module文件夹

--- node_modules/@vue/language-core/lib/codegen/utils/wrapWith.d.ts
+++
@@ -1,2 +0,0 @@
-import type { Code, VueCodeInformation } from '../../types';
-export declare function wrapWith(source: string, startOffset: number, endOffset: number, features: VueCodeInformation, ...codes: Code[]): Generator<Code>;
